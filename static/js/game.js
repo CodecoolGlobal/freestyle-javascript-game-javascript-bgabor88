58a6d3b23fda5dbf3f5c33e41038c5867636a235
galambati <galambosati98@gmail.com>
2021-10-14 11:11:48 +0200

    update button styling

--- static/js/game.js
+++ static/js/game.js
@@ -97,12 +97,12 @@ function resetBoard() {
     [firstCard, secondCard] = [null, null];
 }
 
-(function shuffle() {
-    cards.forEach(card => {
-        let randomPos = Math.floor(Math.random() * 12);
-        card.style.order = randomPos.toString();
-    });
-})();
+// (function shuffle() {
+//     cards.forEach(card => {
+//         let randomPos = Math.floor(Math.random() * 12);
+//         card.style.order = randomPos.toString();
+//     });
+// })();
 
 let watch = setInterval(function() {
   clock();
