d4fbc045e4f9d2e8474d99e98ca986ac7d4e20c7
richardmogyoro <richard.mogyoro@gmail.com>
2021-10-14 14:42:39 +0200

    add first part of tutorial

--- static/js/tutorial.js
+++ static/js/tutorial.js
@@ -1,6 +1,4 @@
-const easyCards = document.querySelectorAll('.memory-card-easy');
-const normalCards = document.querySelectorAll('.memory-card-normal');
-const hardCards = document.querySelectorAll('.memory-card-hard');
+const cards = document.querySelectorAll('.memory-card-easy');
 
 let hasFlippedCard = false;
 let lockBoard = false;
@@ -9,15 +7,6 @@ let score = 0;
 let secondCounter = 0;
 let minuteCounter = 0;
 
-if (easyCards.length !== 0) {
-    cards = easyCards;
-}
-if (normalCards.length !== 0) {
-    cards = normalCards;
-}
-if (hardCards.length !== 0) {
-    cards = hardCards;
-}
 
 clock()
 
@@ -51,9 +40,9 @@ function disableCards() {
 }
 
 function scoring() {
-    if (minuteCounter === 0 && secondCounter <= 20){
+    if (minuteCounter === 0 && secondCounter <= 20) {
         score += 10;
-    } else if (minuteCounter === 0 && secondCounter > 20 && secondCounter <= 40){
+    } else if (minuteCounter === 0 && secondCounter > 20 && secondCounter <= 40) {
         score += 8;
     } else {
         score += 5;
@@ -76,14 +65,14 @@ function resetBoard() {
 }
 
 
-let watch = setInterval(function() {
-  clock();
+let watch = setInterval(function () {
+    clock();
 }, 1000);
 
 function clock() {
     let time = new Date();
     secondCounter += 1;
-    if (secondCounter === 60){
+    if (secondCounter === 60) {
         secondCounter -= 60;
         minuteCounter += 1;
     }
@@ -93,5 +82,81 @@ function clock() {
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
-document.body.style.background = 'url(/static/assets/tutorial.jpg), no-repeat center center fixed';
-document.body.style.backgroundSize = 'cover'
+// Tutorial content
+
+let firstClickFinished = false
+let secondClickFinished = false
+
+function firstClick() {
+    firstClickFinished = true
+    Swal.fire({
+        title: 'Good job!',
+        text: 'Now click on the one BELOW it.',
+        confirmButtonText: 'Got it!'
+    })
+    setInterval(function(){
+        if (secondClickFinished == false) {
+            if (cards[5].style.visibility === 'hidden') {
+                cards[5].style.visibility = 'visible';
+            } else {
+                cards[5].style.visibility = 'hidden';
+            }
+        }
+    }, 1000);
+    cards[5].addEventListener('click', secondClick);
+}
+
+function secondClick() {
+    secondClickFinished = true
+    Swal.fire({
+        title: 'Bad luck!',
+        text: 'These were not a pair. Click on the first one again!',
+        confirmButtonText: 'Sure!'
+    })
+}
+
+window.onload = (event) => {
+    document.body.style.background = 'url(/static/assets/tutorial.jpg), no-repeat center center fixed';
+    document.body.style.backgroundSize = 'cover'
+    Swal.fire({
+        title: 'Welcome to the tutorial!',
+        text: 'In this tutorial, you will learn the basics of Game of Pairs.',
+        confirmButtonText: 'I am your Padawan!'
+    })
+        .then((result) => {
+            Swal.fire({
+                title: 'Flipping a card',
+                text: 'Click on the first card to reveal it´s content.',
+                confirmButtonText: 'Yes, Master Yoda!'
+            })
+            setInterval(function(){
+                if (firstClickFinished == false) {
+                    if (cards[0].style.visibility === 'hidden') {
+                        cards[0].style.visibility = 'visible';
+                    } else {
+                        cards[0].style.visibility = 'hidden';
+                    }
+                }
+            }, 1000);
+    })
+    cards[0].addEventListener('click', firstClick);
+
+    /*new Promise((resolve, reject) => {
+        document.body.style.background = 'url(/static/assets/tutorial.jpg), no-repeat center center fixed';
+        document.body.style.backgroundSize = 'cover'
+        resolve()
+    })
+        .then(() => {
+            let modalsIntro = [];
+            modalsIntro.push({
+                title: 'Welcome to the tutorial!',
+                text: 'In this tutorial, you will learn the basics of Game of Pairs.'
+            });
+            modalsIntro.push({title: 'Flipping a card', text: 'Click on the first card to reveal it´s content.'});
+            Swal.queue(modalsIntro);
+        });
+
+     */
+};
+
+
